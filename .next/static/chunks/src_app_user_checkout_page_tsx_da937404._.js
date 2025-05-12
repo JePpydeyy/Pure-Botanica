afbca8a4b35850288caf576344ba12ff925e82a0
JePpydeyy <tisz1612005@gmail.com>
2025-05-12 12:59:28 +0700

    Remove .next from tracking

--- .next/static/chunks/src_app_user_checkout_page_tsx_da937404._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_30e862d7._.js",
-  "static/chunks/src_app_user_checkout_b3080ecf._.js",
-  "static/chunks/src_app_user_checkout_checkout_module_5646080d.css"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_user_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f5914783._.js",
-  "static/chunks/[root-of-the-server]__59bb3532._.css"
-],
-    source: "dynamic"
-});

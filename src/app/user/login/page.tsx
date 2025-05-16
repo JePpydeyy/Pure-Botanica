849187485b03bfa72d09330ee17b20b878eb7aec
bear080604 <phuonganhkhanh683@gmail.com>
2025-05-16 11:39:57 +0700

    chỉnh lại địa chỉ api cho đăng nhập

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
     setError(""); // Reset error
 
     try {
-      const res = await fetch("http://localhost:10000/api/users/login", {
+      const res = await fetch("https://api-zeal.onrender.com/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
